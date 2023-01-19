8ae84702ea1efb90ad7b4d60edf19ef96c79e560
CCD <dahon0001@gmail.com>
2023-01-19 11:35:44 +0800

    upload Cu surface ver and modify inital ver

--- random_setposition_Cusurface.py
+++ random_setposition_Cusurface.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 #  This script was made by CCD
-#  AIRSS_v3.0_20220525
+#  AIRSS_v4.0_20220816
 #  Condensed Matter Physics Group
 
 #載入函數
 from ase import Atom,Atoms
+from ase.constraints import FixAtoms
 from ase.io.vasp import write_vasp,read_vasp
-from ase.build import sort
+from ase.build import sort, add_vacuum
 from random import shuffle
 import numpy as np
 import math
@@ -24,8 +25,8 @@ class Atoms_random(Atoms):
 
 	#兩direct位置距離
 	def get_distance(positions1, positions2, cell):
-		vec = abs(positions1 - positions2)					#兩位置向量差
-		for i in range(3):							#調整週期性
+		vec = abs(positions1 - positions2)						#兩位置向量差
+		for i in range(3):
 			if vec[i] > 0.5:
 				vec[i] -= 1
 		dot = np.dot(vec, cell)							#轉成Cartesian
@@ -55,30 +56,23 @@ class Atoms_random(Atoms):
 			shuffle(append_list)
 
 			#建構結構
-			rs = Atoms(cell = ini_cell)
+			rs = Atoms(append_list[0] + append_list[1], scaled_positions = [(0.0, 0.0, 0.0), (0.5, 0.5, 0.0)], cell = [3.6153800488, 3.6153800488, 5])
 
 			#加入原子與位置
-			for elements in append_list:
+			for elements in append_list[2:]:
 				condition = 0						#重置while True
 				times = 0
 
-				#如果rs是空的，在原點添加元素
-				if len(rs.get_scaled_positions()) == 0:
-					rs.append(elements)
-					#print("first, ok")
-					#print("now rs", rs, rs.get_scaled_positions())
-					continue					#跳過後續進入下一個for迴圈
-
 				#生成亂數並判斷距離
 				while condition == 0:					#無限迴圈
-					random = np.random.rand(3)			#生成[1*3]亂數(位置)
+					random_position = np.random.rand(3)			#生成[1*3]亂數(位置)
 					max_dis_times = 0				#小於最大距離次數
 					times +=1
 					#print(elements, times, "times random")
 							
 					#判斷距離
 					for r_pos in range(len(rs.get_scaled_positions())):
-						distance = Atoms_random.get_distance(random, rs.get_scaled_positions()[r_pos], rs.get_cell())
+						distance = Atoms_random.get_distance(random_position, rs.get_scaled_positions()[r_pos], rs.get_cell())
 						#print(r_pos, "r_pos:", rs.get_scaled_positions()[r_pos], "random:", random, "distance:", distance)
 							
 						#距離太近
@@ -96,11 +90,13 @@ class Atoms_random(Atoms):
 							#print("all positions are checked")
 
 							if max_dis_times > 0:									#確認最近原子距離在最大距離內
-								atom2 = Atoms(elements, scaled_positions = random, cell = rs.get_cell())	#添加元素Atoms
-								rs += atom2									#加到rs(原來的Atoms)
-								rs.center(vacuum = add_vac)							#將cluster移到中心並加上真空層
-								rs = sort(rs)									#sort symbol
+								rs.append(elements)
+								rs[-1].scaled_position = random_position
+								rs.center(vacuum = 0, axis = 2)									#將cluster移到中心並加上真空層
+								add_vacuum(rs, 5)							#將cluster移到中心並加上真空層
+								#rs = sort(rs)									#sort symbol
 								condition += 1									#使while False，打斷迴圈
+								#write_vasp(str(rs.symbols) + "_" + "{:02d}".format(number + 1) + ".vasp", rs, direct=True, sort=True, vasp5=True)
 								#print("ok, < max", max_dis_times, "times")
 
 							else:				#最近原子不在最大距離內
@@ -111,9 +107,20 @@ class Atoms_random(Atoms):
 			#Atoms_random.check_distance(rs.get_scaled_positions(), rs.get_cell())
 
 			#調整真空層
-			rs.center(vacuum = final_vac)
+			rs.center(vacuum = 0, axis = 2)
+			add_vacuum(rs, 3)
+			top_layer = Atoms(rs.symbols[0] + rs.symbols[1], scaled_positions = [(0.0, 0.0, 1.0), (0.5, 0.5, 1.0)], cell= rs.get_cell())
+			rs += top_layer
+			rs.center(vacuum = 3, axis=2)
+			Cu = Atoms("Cu2", scaled_positions = [(0.5, 0.0, 0.0), (0.0, 0.5, 0.0)], cell = rs.get_cell())
+			rs += Cu
+			rs.set_constraint(FixAtoms(indices=[atom.index for atom in rs if atom.position[2] < 0.05]))
+			rs = sort(rs)
+
 
 			#輸出vasp檔
 			subprocess.run("mkdir " + str(rs.symbols) + "_" + "{:02d}".format(number + 1), shell = True)	#創建資料夾
-			write_vasp(str(rs.symbols) + "_" + "{:02d}".format(number + 1) + "/POSCAR", rs, direct=True, sort=True, vasp5=True)
+			write_vasp(str(rs.symbols) + "_" + "{:02d}".format(number + 1) + "/POSCAR_0", rs, direct=True, sort=True, vasp5=True)
 			print(number + 1, "complete")
+
+Atoms_random.random(40, ["Cr", "Cu"], [3, 1])
